ef9b36035edf4f79653ef29a4dca325f3aaad2ec
ykeanu <ykhernan@ucla.edu>
2017-11-19 21:57:30 -0800

    Refactor code

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,9 +1,9 @@
-const friends = require("../data/friends.js");
+const friendsList = require("../data/friends.js");
 
+// ============ ROUTING ============
 module.exports = function(app) {
-
     app.get("/api/friends", function(req, res) {
-        res.json(friends);
+        res.json(friendsList);
     });
 
     app.post("/api/friends", function(req, res) {
@@ -12,31 +12,28 @@ module.exports = function(app) {
             photo: "",
             friendDifference: 1000
         };
-        console.log(reg.body);
 
         let userData = req.body;
         let userScores = userData.score;
 
-        console.log(userScores);
-
         let totalDifference = 0;
 
         // Nested for loops + conditionals
-        for (var i=0; i < friends.lenth; i++) {
-            console.log(friends[i]);
+        for (var i=0; i < friendsList.lenth; i++) {
+            console.log(friendsList[i]);
             totalDifference = 0;
 
-            for (var j = 0; j < friends[i].scores[j]; j++) {
-                totalDifference += Math.abs(parseInt(userScores[j]) - parseInt(friends[i].scores[j]));
+            for (var j = 0; j < friendsList[i].scores[j]; j++) {
+                totalDifference += Math.abs(parseInt(userScores[j]) - parseInt(friendsList[i].scores[j]));
 
                 if (totalDifference <= bestMath.friendDifference) {
-                    bestMatch.name = friends[i].name;
-                    bestMatch.photo = friends[i].photo;
+                    bestMatch.name = friendsList[i].name;
+                    bestMatch.photo = friendsList[i].photo;
                     bestMatch.friendDifference = totalDifference;
                 }
             }
         }
-        friends.push(userData);
+        friendsList.push(userData);
 
         res.json(bestMatch);
     });
